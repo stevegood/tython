204b845cb3032205e0a3db9c5b6ed25160614a76
Steve Good <sgood@lanctr.com>
2017-07-28 11:41:48 -0700

    Added list endpoint

--- routes/actions.js
+++ routes/actions.js
@@ -2,6 +2,19 @@ import { fix_ints } from '../db/neo4j'
 
 const actions = (app, get_session) => {
   app.route('/actions')
+    .get((req, res) => {
+      const session = get_session()
+      
+      session
+        .run('MATCH (action:Action) RETURN action')
+        .then(result => {
+          const actions = result.records
+            .map(record => fix_ints(record.get('action').properties))
+            
+          session.close()
+          res.send({actions})
+        })
+    })
   
   app.route('/actions/:id')
 }
