7397375cfc25693d9c6df4110e04e9b4e5832ca3
Steve Good <sgood@lanctr.com>
2017-07-28 12:12:15 -0700

    Added endpoint /slots/:id that returns slot + upgrades. Closes #9.

--- routes/slots.js
+++ routes/slots.js
@@ -19,6 +19,35 @@ const slots = (app, get_session) => {
     })
   
   app.route('/slots/:id')
+    .get((req, res) => {
+      const session = get_session()
+      const id = req.params.id
+      
+      session
+        .run(`
+          MATCH (slot:Slot{id: $id})<-[:Fits]-(upgrade:Upgrade)
+          RETURN slot, upgrade`,
+          {id}
+        )
+        .then(result => {
+          if (result.records.length > 0) {
+            
+            const slot = result.records[0].get('slot').properties
+            const upgrades = result.records
+              .map(record => fix_ints(record.get('upgrade').properties))
+            
+            session.close()
+            res.send(fix_ints({
+              ...slot,
+              upgrades
+            }))
+            
+          } else {
+            session.close()
+            res.status(404).end()
+          }
+        })
+    })
 }
 
 export default slots
