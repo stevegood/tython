0abbd8e2eba4b84b97ff4551aaee16e154250027
Steve Good <sgood@lanctr.com>
2017-07-28 11:50:51 -0700

    Added route for /actions/:id. Returns action + ships. Closes #8.

--- routes/actions.js
+++ routes/actions.js
@@ -17,6 +17,33 @@ const actions = (app, get_session) => {
     })
   
   app.route('/actions/:id')
+    .get((req, res) => {
+      const session = get_session()
+      const id = req.params.id
+      
+      session
+        .run(`
+          MATCH (action:Action{id: $id})<-[:Performs]-(ship:Ship)
+          RETURN action, ship`,
+          {id}
+        )
+        .then(result => {
+          if (result.records.length > 0) {
+            const action = result.records[0].get('action').properties
+            const ships = result.records
+              .map(record => fix_ints(record.get('ship').properties))
+              
+            session.close()
+            res.send(fix_ints({
+              ...action,
+              ships
+            }))
+          } else {
+            session.close()
+            res.status(404).end()
+          }
+        })
+    })
 }
 
 export default actions
